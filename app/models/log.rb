d1a579dc5aae12217dc8ca27d6ad59f1d3f1487f
grey <permale.rey@gmail.com>
2022-08-04 13:47:45 +1000

    played with tags for multiple service concept

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,6 @@
 class Log < ApplicationRecord
     belongs_to :service, optional: true
+
+    TAGS = %i[in out in-and-out]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_31_022415) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_025237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_31_022415) do
     t.text "status"
     t.date "dateinstructed"
     t.integer "service_id"
+    t.text "tags", default: [], array: true
   end
 
   create_table "services", force: :cascade do |t|
