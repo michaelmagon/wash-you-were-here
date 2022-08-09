73ab059d1c09947ad35aaaf3960310daab748a35
grey <permale.rey@gmail.com>
2022-08-10 03:59:17 +1000

    added dom_id method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_07_035835) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_175055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_07_035835) do
     t.integer "price"
     t.float "rate"
     t.integer "quantity"
-    t.bigint "service_id"
   end
 
 end
