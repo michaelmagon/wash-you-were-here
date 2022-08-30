1e2655d06b2a5cda770d0a6f77c1fa842d520126
grey <permale.rey@gmail.com>
2022-08-31 00:11:20 +1000

    added validates for service page

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,6 @@
 class Service < ApplicationRecord
     has_many :availed_services
     has_many :logs, through: :availed_services
+
+    validates :title, :body, :quantity, :rate, :description, :presence => true
 end
