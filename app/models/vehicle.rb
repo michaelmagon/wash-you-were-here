2331f7d859a8a720b20c8376504164980c09d0eb
grey <permale.rey@gmail.com>
2022-08-31 00:14:36 +1000

    vehicle validates

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,6 +2,8 @@ class Vehicle < ApplicationRecord
     belongs_to :customer,  optional: true
     has_many :logs
 
+    validates :make, :model, :color, :customer_id, :plates, :description, :presence => true
+
     def display_model
         "#{model}"
     end
