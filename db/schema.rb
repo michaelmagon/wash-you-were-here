55956ba4c2d2372e30a2731652ce7db738012ce9
grey <permale.rey@gmail.com>
2022-09-22 09:32:33 +1000

    phone number to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_123244) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_21_231824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,8 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_123244) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.integer "phone_number"
-    t.integer "secondary_phone_number"
+    t.string "phone_number"
+    t.string "secondary_phone_number"
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
