23b227c02b7bbbe95403b27eb529713e56a0578d
Michael Francis Magon <58822035+michaelmagon@users.noreply.github.com>
2022-09-22 22:15:49 +0800

    Update 20220906144920_change_data_type_for_status.rb

--- db/migrate/20220906144920_change_data_type_for_status.rb
+++ db/migrate/20220906144920_change_data_type_for_status.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeForStatus < ActiveRecord::Migration[7.0]
   def change
-    change_column :logs, :status, :integer, using: 'status::integer', default: 0
+    change_column :logs, :status, :integer, default: 0
   end
 end
